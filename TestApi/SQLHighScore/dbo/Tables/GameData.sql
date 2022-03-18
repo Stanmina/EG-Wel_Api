48a7d424595bb0fc632b688a5aa698dc2b5870a9
Stanmina <stan.vdm.2003@gmail.com>
2022-03-18 01:05:30 +0100

    Verwijderen van oude Database

--- TestApi/SQLHighScore/dbo/Tables/GameData.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[GameData]
-(
-	[Id] INT NOT NULL PRIMARY KEY , 
-    [UserDataId] INT NOT NULL, 
-    [HighScore] INT NULL, 
-    [LevelsCompleted] INT NULL, 
-    [TimePlayedInSeconds] INT NULL, 
-    CONSTRAINT [FK_GameData_UserData] FOREIGN KEY ([UserDataId]) REFERENCES [UserData]([Id])
-)
--- TestApi/SQLHighScore/dbo/Tables/UserData.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[UserData]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NULL, 
-    [Alias] NVARCHAR(50) NULL
-)
